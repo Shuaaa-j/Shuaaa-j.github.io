d1ef00680319b3356409d168eb03dca9345a78f2
Shuaaa-j <JoshuaGofulco246@gmail.com>
2024-06-02 23:09:18 -0700

    news.js

---
+++ news.js
@@ -0,0 +1,9 @@
+/* More News Button Scroll */
+var moreNewsButton = document.querySelector('.cta-button');
+var esportsSection = document.querySelector('.esports-section');
+
+moreNewsButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    esportsSection.scrollIntoView({ behavior: 'smooth' });
+});
+
